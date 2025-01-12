a86833a0fccd1bc4331e88b49ca1e6164dc34539
Udayan Basak <udayan.santu@gmail.com>
2025-01-13 01:13:11 +0600

    1

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const cors = require('cors');
+
+app.use(cors({ origin: "*" }));
+
+app.get("/", async(req, res) => {
+    res.send('server is running 1');
+});
+
+app.listen(3000, ()=> console.log('Server is running on 3000'))
