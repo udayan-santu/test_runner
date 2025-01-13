564014ac324ca41d99802e05f58e1b13e4aeadeb
Udayan Basak <41102518+udayan-santu@users.noreply.github.com>
2025-01-13 13:46:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 app.use(cors({ origin: "*" }));
 
 app.get("/", async(req, res) => {
-    res.send('server is running 2');
+    res.send('server is running 3');
 });
 
 app.listen(3000, ()=> console.log('Server is running on 3000'))
